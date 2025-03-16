402a78c09af74b06544eb1b123c6fe5ae7c72b53
Sari Hammad <sari_hammad@hotmail.com>
2025-03-16 19:10:28 -0400

    feat: add Maven Wrapper and initial test

---
+++ src/test/java/com/example/urlshortener/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = UrlShortenerApplication.class)
+public class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test simply checks if the context loads
+    }
+}
