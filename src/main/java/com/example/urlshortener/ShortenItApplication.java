39dde5cb21306b7791350893166106716a2b9236
Sari Hammad <sari_hammad@hotmail.com>
2025-03-22 15:54:54 -0400

    refactor: rename application class

--- src/main/java/com/example/urlshortener/ShortenItApplication.java
+++ src/main/java/com/example/urlshortener/ShortenItApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlShortenerApplication {
+public class ShortenItApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrlShortenerApplication.class, args);
+		SpringApplication.run(ShortenItApplication.class, args);
 	}
 
 }
