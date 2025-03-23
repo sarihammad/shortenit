0e2515e2cf2eb9b389270272f385b9b7bb32c129
Sari Hammad <sari_hammad@hotmail.com>
2025-03-22 21:52:47 -0400

    feat: add main application class

---
+++ src/main/java/com/example/shortenit/ShortenItApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortenit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenItApplication.class, args);
+	}
+
+}
