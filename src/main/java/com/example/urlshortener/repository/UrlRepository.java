0df4e58dc3ff86bfe104fbe61940c9d921cbf338
Sari Hammad <sari_hammad@hotmail.com>
2025-03-22 21:52:25 -0400

    refactor: update application name

--- src/main/java/com/example/urlshortener/repository/UrlRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.urlshortener.repository;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-import com.example.urlshortener.model.UrlMapping;
-
-public interface UrlRepository extends CassandraRepository<UrlMapping, String> {
-    
-}
